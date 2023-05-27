15b0eea9b4a71a4050ce3cbcfb462e6fe2bc1e09
sufi16061180 <81066177+sufi16061180@users.noreply.github.com>
2023-05-27 20:43:11 +0600

    Update app.js

--- app.js
+++ app.js
@@ -9,29 +9,7 @@ const setRoutes = require('./routes/routes');
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const dbURI= 'mongodb+srv://nuren1180:nuren1180@taskferi.0bhgc.mongodb.net/?retryWrites=true&w=majority';
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+const dbURI= 'mongodb+srv://nuren1180:nuren1180@taskferi.0bhgc.mongodb.net/';
 
 
 const app=express();
